17038bc49322b98c1246f4f10163d1d9fc084d95
ArturoMetalhead <perezcabreracarlosarturo@gmail.com>
2024-04-08 18:30:02 -0400

    recuperacion de evaluador

--- HulkGrammar.py
+++ HulkGrammar.py
@@ -1,6 +1,7 @@
 from Grammar import *
 from Lexer import *
 from Semantic_checker import *
+import math
 
 
 def HulkGrammar():
@@ -29,6 +30,9 @@ def HulkGrammar():
     args, args_aux, args_in_par = G.NonTerminals('<args> <args-aux> <args-in-par>')
     expr_dot = G.NonTerminal('<expr-dot>')
     negative = G.NonTerminal('<negative>')
+    let_optional = G.NonTerminal('<let-optional>')
+    block_expr_semi_list = G.NonTerminal('<block-expr-semi-list>')
+    
 
     type_, inherits, extends, protocol, new_ = G.Terminals('type inherits extends protocol new')
     function_, arrow = G.Terminals('function =>')
@@ -52,42 +56,47 @@ def HulkGrammar():
     program %= program + function_stat , lambda h,s: ProgramNode(s[1],s[2])
     program %= program + protocol_stat , lambda h,s: ProgramNode(s[1],s[2])
     program %= program + type_stat , lambda h,s: ProgramNode(s[1],s[2])
-    program %= G.Epsilon  , lambda h,s: ProgramNode([],[])
+    program %= program + okey + block_expr_semi_list + ckey , lambda h,s: ProgramNode(s[1],s[3])
+    program %= G.Epsilon  , lambda h,s: ProgramNode(None,None)
 
-    function_stat %= function_ + id_ + opar + params + cpar + id_extend + body ,lambda h,s: FunctionStatNode(s[1],s[2],s[4],s[6],s[7])
+    block_expr_semi_list %= block_expr_semi_list + okey + block_expr_semi_list + ckey, lambda h,s : BlockExprListNode(s[1],s[3])
+    block_expr_semi_list %= block_expr_semi_list + expr + semi , lambda h,s : BlockExprListNode(s[1],[s[2]])
+    block_expr_semi_list %= G.Epsilon , lambda h,s : BlockExprListNode(None,None)
+    
+    function_stat %= function_ + id_ + opar + params + cpar + id_extend + body ,lambda h,s: FunctionStatNode(s[2],s[4],s[6],s[7])
 
-    type_stat %= type_ + id_ + params_in_par + inherits_expr + okey + decls_methods_semi  + ckey , lambda h,s: TypeStatNode(s[1],s[2],s[4],s[6])
+    type_stat %= type_ + id_ + params_in_par + inherits_expr + okey + decls_methods_semi  + ckey , lambda h,s: TypeStatNode(s[2],s[4],s[6])
 
-    protocol_stat %= protocol + id_ + extends_expr + okey + method_protocol_list + ckey , lambda h,s: ProtocolStatNode(s[1],s[2],s[4],s[6])
+    protocol_stat %= protocol + id_ + extends_expr + okey + method_protocol_list + ckey , lambda h,s: ProtocolStatNode(s[2],s[4],s[6])
 
     method_protocol %= id_ + opar + params + cpar + id_extend  , lambda h,s: MethodProtocolNode(s[1],s[3],s[5])
 
     method_protocol_list %= method_protocol_list + method_protocol + semi , lambda h,s: s[1] + [s[2]]
     method_protocol_list %= method_protocol + semi , lambda h,s: [s[1]]
 
-    extends_expr %= extends + id_  , lambda h,s: ExtendsExprNode(s[1], s[2])
-    extends_expr %= G.Epsilon , lambda h,s: ExtendsExprNode(None, None)
+    extends_expr %= extends + id_  , lambda h,s: ExtendsExprNode( s[2])
+    extends_expr %= G.Epsilon , lambda h,s: ExtendsExprNode(None)
 
-    inherits_expr %= inherits + id_ + args_in_par  , lambda h,s: InheritsExprNode(s[1], s[2], s[3])
-    inherits_expr %= G.Epsilon , lambda h,s: InheritsExprNode(None, None, None)
+    inherits_expr %= inherits + id_ + args_in_par  , lambda h,s: InheritsExprNode( s[2], s[3])
+    inherits_expr %= G.Epsilon , lambda h,s: InheritsExprNode( None, None)
 
     decls_methods_semi %= decls_methods_semi + decl + semi , lambda h,s: s[1] + [s[2]] ##
     decls_methods_semi %= decls_methods_semi + method , lambda h,s: s[1] + [s[2]] ##
-    decls_methods_semi %= G.Epsilon , lambda h,s: []
+    decls_methods_semi %= G.Epsilon , lambda h,s: None
 
     method %= id_ + opar + params + cpar + id_extend + body , lambda h,s: MethodNode(s[1],s[3],s[5],s[6])
 
     body %= arrow + expr + semi , lambda h,s: BodyNode(s[2])
     body %= okey + expr_list_semi + ckey , lambda h,s: BodyNode(s[2])
 
-    params_aux %= id_ + id_extend  , lambda h,s: ParamsAuxNode(s[1],s[2])
-    params_aux %= params_aux + comma + id_ + id_extend, lambda h,s: s[1] + [ParamsAuxNode(s[3],s[4])]
+    params_aux %= id_ + id_extend  , lambda h,s: [ParamsAuxNode(None,s[1],s[2])]
+    params_aux %= params_aux + comma + id_ + id_extend, lambda h,s:s[1]+[ParamsAuxNode(s[1],s[3],s[4])]
 
     params %= params_aux , lambda h,s: ParamsNode(s[1])
-    params %= G.Epsilon , lambda h,s: ParamsNode([])
+    params %= G.Epsilon , lambda h,s: ParamsNode(None)
 
     params_in_par %= opar + params + cpar , lambda h,s: ParamsInParNode(s[2])
-    params_in_par %= G.Epsilon , lambda h,s: ParamsInParNode([])
+    params_in_par %= G.Epsilon , lambda h,s: ParamsInParNode(None)
 
     expr %= let_expr ,lambda h,s: s[1]
     expr %= if_expr ,lambda h,s: s[1]
@@ -107,6 +116,9 @@ def HulkGrammar():
 
     let_expr %= let + decls + in_ + expr_body , lambda h,s: LetExprNode(s[2],s[4])
 
+    let_optional %= let , lambda h,s: LetOptionalNode(s[1])
+    let_optional %= G.Epsilon , lambda h,s: LetOptionalNode(None)
+
     destr_expr %= loc + destr + expr , lambda h,s: DestrExprNode(s[1],s[3])
 
     while_expr %=  while_ + opar + expr + cpar + expr_body , lambda h,s: WhileExprNode(s[3],s[5])
@@ -116,88 +128,88 @@ def HulkGrammar():
     if_expr %= if_ + opar + expr + cpar + expr_body + elif_expr , lambda h,s: IfExprNode(s[3],s[5],s[6])
 
     elif_expr %= elif_ + opar + expr + cpar + expr_body + elif_expr , lambda h,s: ElifExprNode(s[3],s[5],s[6])
-    elif_expr %= else_expr , lambda h,s: s[1]
+    elif_expr %= else_expr , lambda h,s: s[1] #########################3
 
     else_expr %= else_ + expr_body , lambda h,s: ElseExprNode(s[2])
 
     decl %= id_ + id_extend + asign + expr , lambda h,s: DeclNode(s[1],s[2],s[4])
 
-    decls %= decls + comma + decl , lambda h,s: s[1] + [s[3]]
+    decls %= decls + comma + let_optional+ decl , lambda h,s: s[1] + [s[4]] ############
     decls %= decl , lambda h,s: [s[1]]
 
     expr_body %= expr  , lambda h,s: ExprBodyNode(s[1])
     expr_body %= okey + expr_list_semi + ckey , lambda h,s: ExprBodyNode(s[2])
 
-    expr_list_semi %= expr_list_semi + expr + semi , lambda h,s: ExprListSemiNode(s[1] + [s[2]])
-    expr_list_semi %= expr + semi , lambda h,s: ExprListSemiNode([s[1]])
+    expr_list_semi %= expr_list_semi + expr + semi , lambda h,s: ExprListSemiNode(s[1],s[2])
+    expr_list_semi %= expr + semi , lambda h,s: ExprListSemiNode(None,s[1])
 
     id_extend %= double_dot + id_ , lambda h,s: IdExtendNode(s[2])
     id_extend %= G.Epsilon , lambda h,s: IdExtendNode(None)
 
-    expr_elem %= expr_elem + is_ + as_expr , lambda h,s: ExprElemNode(s[1],s[3])
-    expr_elem %= as_expr  , lambda h,s: ExprElemNode(s[1],None)
+    expr_elem %= expr_elem + is_ + as_expr , lambda h,s: ExprElemNode(s[1],s[3], True)
+    expr_elem %= as_expr  , lambda h,s: ExprElemNode(s[1],None, False)
 
     as_expr %= as_expr + as_ + logic_concat_expr , lambda h,s: AsExprNode(s[1],s[3])
     as_expr %= logic_concat_expr , lambda h,s: AsExprNode(s[1],None)
 
-    logic_concat_expr %= logic_concat_expr + arroba + comp_expr , lambda h,s: LogicConcatExprNode(s[1],s[3])
-    logic_concat_expr %= logic_concat_expr + double_arroba + comp_expr , lambda h,s: LogicConcatExprNode(s[1],s[3])
-    logic_concat_expr %= logic_concat_expr + and_ + comp_expr , lambda h,s: LogicConcatExprNode(s[1],s[3])
-    logic_concat_expr %= logic_concat_expr + or_ + comp_expr , lambda h,s: LogicConcatExprNode(s[1],s[3])
-    logic_concat_expr %= not_ + comp_expr , lambda h,s: LogicConcatExprNode(s[2],None)
-    logic_concat_expr %= comp_expr , lambda h,s: LogicConcatExprNode(s[1],None)
+    logic_concat_expr %= logic_concat_expr + arroba + comp_expr , lambda h,s: LogicConcatExprNode(s[1],s[3],"@")
+    logic_concat_expr %= logic_concat_expr + double_arroba + comp_expr , lambda h,s: LogicConcatExprNode(s[1],s[3], "@@")
+    logic_concat_expr %= logic_concat_expr + and_ + comp_expr , lambda h,s: AndNode(s[1],s[3])
+    logic_concat_expr %= logic_concat_expr + or_ + comp_expr , lambda h,s: OrNode(s[1],s[3])
+    logic_concat_expr %= not_ + comp_expr , lambda h,s: NotNode(s[2])
+    logic_concat_expr %= comp_expr , lambda h,s: LogicConcatExprNode(s[1],None,None)
 
-    comp_expr %= comp_expr + equals + aritm_expr , lambda h,s: CompExprNode(s[1],s[3])
-    comp_expr %= comp_expr + not_equals + aritm_expr , lambda h,s: CompExprNode(s[1],s[3])
-    comp_expr %= comp_expr + greater + aritm_expr , lambda h,s: CompExprNode(s[1],s[3])
-    comp_expr %= comp_expr + greater_equals + aritm_expr , lambda h,s: CompExprNode(s[1],s[3])
-    comp_expr %= comp_expr + less + aritm_expr , lambda h,s: CompExprNode(s[1],s[3])
-    comp_expr %= comp_expr + less_equals + aritm_expr , lambda h,s: CompExprNode(s[1],s[3])
+    comp_expr %= comp_expr + equals + aritm_expr , lambda h,s: EqualsNode(s[1],s[3])
+    comp_expr %= comp_expr + not_equals + aritm_expr , lambda h,s: NotEqualsNode(s[1],s[3])
+    comp_expr %= comp_expr + greater + aritm_expr , lambda h,s: GreaterNode(s[1],s[3])
+    comp_expr %= comp_expr + greater_equals + aritm_expr , lambda h,s: GreaterEqualsNode(s[1],s[3])
+    comp_expr %= comp_expr + less + aritm_expr , lambda h,s: LessNode(s[1],s[3])
+    comp_expr %= comp_expr + less_equals + aritm_expr , lambda h,s: LessEqualsNode(s[1],s[3])
     comp_expr %= aritm_expr , lambda h,s: CompExprNode(s[1],None)
 
-    aritm_expr %= aritm_expr + plus + term , lambda h,s: AritmExprNode(s[1],s[3]) ##seria sum node
-    aritm_expr %= aritm_expr + minus + term , lambda h,s: AritmExprNode(s[1],s[3]) ##
+    aritm_expr %= aritm_expr + plus + term , lambda h,s: SumNode(s[1],s[3]) ##seria sum node
+    aritm_expr %= aritm_expr + minus + term , lambda h,s: MinusNode(s[1],s[3]) ##
     aritm_expr %= term , lambda h,s: AritmExprNode(s[1],None) ##
 
-    term %= term + star + pow_expr , lambda h,s: TermNode(s[1],s[3]) ##
-    term %= term + div + pow_expr , lambda h,s: TermNode(s[1],s[3]) ##
-    term %= term + mod + pow_expr , lambda h,s: TermNode(s[1],s[3]) ##
+    term %= term + star + pow_expr , lambda h,s: MultNode(s[1],s[3]) ##
+    term %= term + div + pow_expr , lambda h,s: DivNode(s[1],s[3]) ##
+    term %= term + mod + pow_expr , lambda h,s: ModNode(s[1],s[3]) ##
     term %= pow_expr , lambda h,s: TermNode(s[1],None) ##
 
     pow_expr %= pow_expr + pow_ + negative , lambda h,s: PowExprNode(s[1],s[3]) ##
     pow_expr %= pow_expr + pow_2 + negative , lambda h,s: PowExprNode(s[1],s[3]) ##
     pow_expr %= negative , lambda h,s: PowExprNode(s[1],None) ##
 
-    negative %= minus + factor , lambda h,s: NegativeNode(s[2])
-    negative %= factor , lambda h,s: NegativeNode(s[1])
+    negative %= minus + factor , lambda h,s: NegativeNode(s[2], True)
+    negative %= factor , lambda h,s: NegativeNode(s[1], False)
 
-    factor %= opar + expr + cpar , lambda h,s: FactorNode(s[2],None,None)###########Ya factores lo de ponerlo en 1 mas
-    factor %= num , lambda h,s: FactorNode(s[1],None,None)
-    factor %= string , lambda h,s: FactorNode(s[1],None,None)
-    factor %= sqrt + opar + expr + cpar , lambda h,s: FactorNode(s[3],None,None)
-    factor %= sin + opar + expr + cpar , lambda h,s: FactorNode(s[3],None,None)
-    factor %= cos + opar + expr + cpar , lambda h,s: FactorNode(s[3],None,None)
-    factor %= exp + opar + expr + cpar , lambda h,s: FactorNode(s[3],None,None)
-    factor %= log + opar + expr + comma + expr + cpar , lambda h,s: FactorNode(s[3],s[5],None)
-    factor %= rand + opar + cpar , lambda h,s: FactorNode(None,None,None)
-    factor %= e , lambda h,s: FactorNode(None,None,None)
-    factor %= pi , lambda h,s: FactorNode(None,None,None)
+    factor %= opar + expr + cpar , lambda h,s: FactorNode(s[2],None,None)
+    factor %= num , lambda h,s: NumNode(s[1],None, None)
+    factor %= string , lambda h,s: StrNode(s[1])
+    factor %= sqrt + opar + expr + cpar , lambda h,s: NumNode(s[3],None,s[1])
+    factor %= sin + opar + expr + cpar , lambda h,s: NumNode(s[3],None,s[1])
+    factor %= cos + opar + expr + cpar , lambda h,s: NumNode(s[3],None,s[1])
+    factor %= exp + opar + expr + cpar , lambda h,s: NumNode(s[3],None,s[1])
+    factor %= log + opar + expr + comma + expr + cpar , lambda h,s:NumNode(s[3],s[5],s[1])
+    factor %= rand + opar + cpar , lambda h,s: NumNode(None,None,s[1])
+    factor %= e , lambda h,s: NumNode(math.e , None, None)
+    factor %= pi , lambda h,s: NumNode(math.pi , None, None)
     factor %= loc , lambda h,s: FactorNode(s[1],None,None)
-    factor %= oindex + args + cindex  , lambda h,s: FactorNode(s[2],None,None)
-    factor %= oindex + expr + that + params_aux + in_ + expr + cindex , lambda h,s: FactorNode(s[2],s[4],s[6]) 
+    factor %= oindex + args + cindex  , lambda h,s: VectorNode(s[2],None,None)
+    factor %= oindex + expr + that + params_aux + in_ + expr + cindex , lambda h,s: VectorNode(s[2],s[4],s[6]) 
 
     loc %= loc + dot + id_ + args_in_par , lambda h,s: LocNode(s[1],s[3],s[4])
     loc %= loc + oindex + expr + cindex , lambda h,s: LocNode(s[1],s[3],None)
-    loc %= id_ + args_in_par , lambda h,s: LocNode(s[1],s[2],None)
+    loc %= id_ + args_in_par , lambda h,s: LocNode(None, s[1], s[2])
 
     args_aux %= expr  , lambda h,s: ArgsAuxNode(s[1],None)
     args_aux %= args_aux + comma + expr , lambda h,s: ArgsAuxNode(s[1],s[3])
 
-    args %= args_aux , lambda h,s: ArgsNode(s[1],None)
-    args %= G.Epsilon , lambda h,s: ArgsNode(None,None)
+    args %= args_aux , lambda h,s: ArgsNode(s[1])
+    args %= G.Epsilon , lambda h,s: ArgsNode(None)
 
-    args_in_par %= opar + args + cpar , lambda h,s: ArgsInParNode(s[2])
-    args_in_par %= G.Epsilon , lambda h,s: ArgsInParNode(None)
+    args_in_par %= opar + args + cpar , lambda h,s: ArgsInParNode(s[2], True)
+    args_in_par %= G.Epsilon , lambda h,s: ArgsInParNode(None, False)
 
     #Generating Lexer
     nonzero_digits = '|'.join(str(n) for n in range(1,10))
--- Main.py
+++ Main.py
@@ -14,49 +14,59 @@ def init():
     G,lexer= HulkGrammar()
     parser = SLR1Parser(G,True)
 
-    texts =['42;' ,'print(42);','print((((1 + 2) ^ 3) * 4) / 5);','print("Hello World");','print("The message is \"Hello World\"");',
-    'print("The meaning of life is " @ 42);','print(sin(2 * PI) ^ 2 + cos(3 * PI / log(4, 64)));',
-    'function tan(x) => sin(x) / cos(x);','function cot(x) => 1 / tan(x);function tan(x) => sin(x) / cos(x);print(tan(PI) ** 2 + cot(PI) ** 2);',
-    'function operate(x, y) {print(x + y);print(x - y);print(x * y);print(x / y);}',
-    'let msg = "Hello World" in print(msg);','let number = 42, text = "The meaning of life is" in print(text @ number);',
-    'let number = 42 in let text = "The meaning of life is" in print(text @ number);',
-    'let number = 42 in (let text = "The meaning of life is" in (print(text @ number)));',
-    'let a = 6, b = a * 7 in print(b);','let a = 6 in let b = a * 7 in print(b);',
-    'let a = 5, b = 10, c = 20 in {print(a+b);print(b*c);print(c/a);};','let a = (let b = 6 in b * 7) in print(a);',
-    'print(let b = 6 in b * 7);','let a = 20 in {let a = 42 in print(a);print(a);};','let a = 7, a = 7 * 6 in print(a);',
-    'let a = 7 in let a = 7 * 6 in print(a);','let a = 0 in {print(a);a := 1;print(a);};',
-    'let a = 0 in let b = a := 1 in {print(a);print(b);};',
-    'let a = 42 in if (a % 2 == 0) print("Even") else print("odd");'
-    ]
+    texts =['let a = 5 in {a; print(9+a); };']
+
+    parserslist = []
+    operationslist=[]
+    tokenslist = []
 
     for i in texts:
         tokens = lexer(i)
+        tokenslist.append(tokens)
         tokens_type = []
         for j in tokens:
             if j.token_type!='space':
                 tokens_type.append(j.token_type)
         parse,operations = parser(tokens_type, get_shift_reduce=True)
+        parserslist.append(parse)
+        operationslist.append(operations)
 
-    new_tokens = []
+    astlist = []
 
-    for i in tokens:
-        if(i.token_type!='space'):
-            new_tokens.append(i)
+    for j in range(len(parserslist)):
+        new_tokens = []
 
-    #region Semantic Checker
-    ast = evaluate_reverse_parse(parse, operations, new_tokens)
+        for i in tokenslist[j]:
+            if(i.token_type!='space'):
+                new_tokens.append(i)
+
+        #region Semantic Checker
+        ast = evaluate_reverse_parse(parserslist[j], operationslist[j], new_tokens)
+        astlist.append(ast)
+
 
     formatter = FormatVisitor()
-    print(formatter.visit(ast))
+    for i in range(len(astlist)):
+        print(formatter.visit(astlist[i]))
 
-    scope = Scope()
 
-    semantic_checker = SemanticCheckerVisitor()
-    errors = semantic_checker.visit(ast)
-    for i, error in enumerate(errors,1):
-        print(f'{i}.', error)
+    print('Semantic Checker')
+    for i in range(len(astlist)):
+
+        semantic_checker = SemanticCheckerVisitor()
+        errors = semantic_checker.visit(astlist[i])
+        for j, error in enumerate(errors,1):
+            print(texts[i])
+            print(f'{j}.', error)
 
     #endregion
     
+    for i in range(len(astlist)):
+        semantic_checker = SemanticCheckerEvaluate()
+        results = semantic_checker.visit(astlist[i])
+        print(results)
+        # for j, res in enumerate(results,1):
+        #     print(texts[i])
+        #     print(f'{j}.', res)
 
 init()
